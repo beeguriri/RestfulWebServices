befe34136c1a1ef176fcd2f0ed74b066647015f6
beeguriri <kkamboting@gmail.com>
2024-01-06 13:35:02 +0900

    swagger 적용

--- src/main/java/wendy/study/restfulwebservices/bean/User.java
+++ src/main/java/wendy/study/restfulwebservices/bean/User.java
@@ -1,6 +1,7 @@
 package wendy.study.restfulwebservices.bean;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -11,18 +12,24 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 @JsonIgnoreProperties(value = {"password", "ssn"})
+@Schema(description = "사용자 상세 정보를 위한 도메인 객체")
 public class User {
 
+    @Schema(title = "사용자 ID", description = "사용자 ID는 자동 생성 됩니다.")
     private Integer id;
 
+    @Schema(title = "사용자 이름", description = "사용자 이름을 입력 합니다.")
     @Size(min = 2, message = "이름은 2글자 이상 입력 해 주세요.")
     private String name;
 
+    @Schema(title = "사용자 등록일", description = "사용자 등록일을 입력 합니다. 입력 하지 않으면 현재 날짜가 저장 됩니다.")
     @Past(message = "등록일은 미래 날짜를 입력 하실 수 없습니다.")
     private Date joinDate;
 
 //    @JsonIgnore //외부에 노출 하고 싶지 않은 데이터에 설정
+    @Schema(title = "사용자 비밀번호", description = "사용자의 비밀번호를 입력 합니다.")
     private String password;
+    @Schema(title = "사용자 주민번호", description = "사용자의 주민번호를 입력 합니다.")
     private String ssn;
 
 }
--- src/main/java/wendy/study/restfulwebservices/controller/UserController.java
+++ src/main/java/wendy/study/restfulwebservices/controller/UserController.java
@@ -1,5 +1,10 @@
 package wendy.study.restfulwebservices.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.hateoas.EntityModel;
@@ -19,6 +24,7 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "user-controller", description = "일반 사용자 서비스를 위한 컨트롤러") //클래스에 대한 설명을 달 때
 public class UserController {
 
     private final UserDaoService service;
@@ -28,8 +34,18 @@ public class UserController {
         return service.findAll();
     }
 
+    @Operation(summary = "사용자 정보 조회 API", description = "사용자 ID를 이용해서 사용자 상세 정보를 조회 합니다.")
+    @ApiResponses(
+            {
+                    @ApiResponse(responseCode = "200", description = "OK"),
+                    @ApiResponse(responseCode = "400", description = "Bad Request"),
+                    @ApiResponse(responseCode = "404", description = "User Not Found"),
+                    @ApiResponse(responseCode = "500", description = "Internal Server Error"),
+            }
+    )
     @GetMapping("/users/{id}")
-    public EntityModel<User> retrieveUser(@PathVariable int id){
+    public EntityModel<User> retrieveUser(
+            @Parameter(description = "사용자 ID", required = true, example = "1") @PathVariable int id){
 
         User user = service.findOne(id);
 
