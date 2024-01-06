c951ab9c9ac37b2c66800370dbf81680f6e43f28
beeguriri <kkamboting@gmail.com>
2024-01-06 18:26:17 +0900

    자원 변경을 위한 api, security configuration 설정

--- src/main/java/wendy/study/restfulwebservices/controller/UserJPAController.java
+++ src/main/java/wendy/study/restfulwebservices/controller/UserJPAController.java
@@ -1,16 +1,18 @@
 package wendy.study.restfulwebservices.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import wendy.study.restfulwebservices.bean.User;
 import wendy.study.restfulwebservices.exception.UserNotFoundException;
 import wendy.study.restfulwebservices.repository.UserRepository;
 
+import java.net.URI;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -42,5 +44,30 @@ public class UserJPAController {
         entityModel.add(link.withRel("all-users")); //all-users -> http://localhost:8088/users
 
         return entityModel;
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUserById(@PathVariable("id") int id) {
+        userRepository.deleteById(id);
+        // 401Unauthorized
+        // resource의 변경이 일어날 경우
+        // filter chain 추가 필요
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
+
+        if(user.getJoinDate() == null)
+            user.setJoinDate(new Date());
+
+        User savedUser = userRepository.save(user);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
